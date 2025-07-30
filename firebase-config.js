204ea257b3c909a1cf3e55ed4ec84f5429b5d057
Kaleid00s <118693409+Kaleid00s@users.noreply.github.com>
2025-07-30 23:58:55 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCuLlkhZiAd45dWN7ve0joXiqP4t7bGpVg",
+  authDomain: "malfargats-app.firebaseapp.com",
+  projectId: "malfargats-app",
+  storageBucket: "malfargats-app.firebasestorage.app",
+  messagingSenderId: "538124617220",
+  appId: "1:538124617220:web:ea5fc0ad6947653e1e9751",
+  measurementId: "G-66R34PFMMX"
+};
